6704239a95b9cbf238843898a671765a263ee06e
George-SSSS <2017312580122@whu.edu.cn>
2024-01-18 01:57:35 +0000

    fix: Volume pool

--- backend/src/volume/service/pool.py
+++ backend/src/volume/service/pool.py
@@ -1,94 +1,107 @@
 import importlib
-from oslo_config import cfg
 
 from src.utils.singleton import singleton
-import src.volume.db as db
+from src.utils.sqlalchemy import enginefacade
+from src.volume import db
 from src.volume.common import config
 from src.volume.db.models import Pool
-from src.volume.db.context import DBContext
 
 
-# 定义配置组
-backend_group = cfg.OptGroup(name='backend',
-                             title='Backend defaults')
-# 定义配置项
-backend_opts = [
-    cfg.StrOpt('provider', default='rbd',
-               help='Specify the backend provider'),
-]
-
-# 注册配置组和配置项
+# 加载配置信息
 CONF = config.CONF
-CONF.register_group(backend_group)
-CONF.register_opts(backend_opts, group=backend_group)
 
-# 获取所选的后端实现方案
-implementation = CONF.backend.implementation
+# 通过配置信息动态加载对应的类
+if (CONF['volume']['provider'].lower() == 'rbd'):
+    pool_settings = config.RbdSettings()
+else:
+    pool_settings = config.LibvirtSettings()
 
-# 定义配置组
-backend_impl_group = cfg.OptGroup(name=implementation,
-                                  title='Backend Driver Implementation')
-# 定义配置项
-backend_impl_opts = [
-    cfg.StrOpt('pool_driver',
-               default='src.Volume.driver.pool.rbd_pool.RbdPool',
-               help='Specify the pool driver'),
-]
-CONF.register_group(backend_impl_group)
-CONF.register_opts(backend_impl_opts, group=backend_impl_group)
 
-# 获取实现方案中的 module
-pool_impl_module = CONF[implementation].pool_impl_class
-# 获取 module 和 class name
-pool_module_name, pool_class_name = pool_impl_module.rsplit('.', 1)
-# 动态加载实现 module
+pool_driver_path = pool_settings.pool_driver
+pool_module_name, pool_driver_name = pool_driver_path.rsplit('.', 1)
 pool_module = importlib.import_module(pool_module_name)
-# 通过反射获取实现类
-pool_impl_class = getattr(pool_module, pool_class_name)
+pool_driver = getattr(pool_module, pool_driver_name)
 
 
 @singleton
 class PoolService():
 
-    context = DBContext()
+    @enginefacade.transactional
+    def create_pool(self, session, pool_name: str, allocation: int, owner: str):
+        pool = Pool(pool_name, allocation, owner)
+        db.insert(session, pool)
+        return pool
 
-    def create_pool(self, name, allocation, owner):
-        pool = Pool(name, allocation, owner)
-        db.insert(self.context, pool)
-        return pool.id
-
-    def delete_pool_by_id(self, id: str = None):
-        pool = db.select_by_id(self.context, Pool, id)
+    @enginefacade.transactional
+    def delete_pool_by_uuid(self, session, uuid: str = None):
+        pool = db.select_by_uuid(session, Pool, uuid)
         if pool is None:
-            raise Exception(f'cannot find a pool which UUID={id}')
+            raise Exception(f'cannot find a pool which UUID is {uuid}')
+        db.delete(session, pool)
 
-        response = pool_impl_class.delete(pool.name)
-        if response.is_success is True:
-            db.delete(pool)
-
-    def delete_pool_by_condition(self, values: dict):
-        pool_list = db.condition_select(self.context, Pool, values)
+    @enginefacade.transactional
+    def delete_pool_by_condition(self, session, values: dict):
+        pool_list = db.condition_select(session, Pool, values=values)
         if len(pool_list) == 0:
             raise Exception(f'cannot match any pool with {values.__dict__}')
+        db.batch_delete(session, pool_list)
 
-        for pool in pool_list:
-            self.delete_pool_by_id(pool.id)
+    @enginefacade.transactional
+    def get_pool_by_uuid(self, session, uuid: str):
+        return db.select_by_uuid(session, Pool, uuid)
 
-    def get_pool_by_id(self, id):
-        return db.select_by_id(self.context, Pool, id)
+    @enginefacade.transactional
+    def get_pool_by_name(self, session, name: str):
+        return db.select_by_name(session, Pool, name)
 
-    def get_pool_by_name(self, name):
-        return db.select_by_name(self.context, Pool, name)
+    @enginefacade.transactional
+    def list_pools(self, session):
+        pool_list = db.condition_select(session, Pool)
+        return pool_list
 
-    def list_all_pool(self):
-        db.condition_select(self.context, Pool)
-
-    def resize_pool(self, id, new_size):
-        pool = db.select_by_id(self.context, Pool, id)
+    def resize_pool(self, session, uuid: str, new_size: int):
+        pool = db.select_by_uuid(session, Pool, uuid)
         if pool.usage <= new_size:
-            db.condition_update(self.context, Pool, id,
+            db.condition_update(session, Pool, uuid,
                                 {'allocation': new_size})
         else:
             raise Exception(
                 "The new capacity is smaller than the used capacity")
 
+
+# session = enginefacade.get_session()
+# service = PoolService()
+# pool1 = service.create_pool(session,
+#                             pool_name="libvirt_pool",
+#                             allocation=20*1024*1024,
+#                             owner="ZYQ")
+# print(pool1.to_dict())
+
+# libvirt_pool = service.get_pool_by_name(session, "libvirt_pool")
+# print(libvirt_pool.to_dict())
+
+
+# pool2 = service.create_pool(session,
+#                             pool_name='pool2',
+#                             allocation=20*1024*1024,
+#                             owner='XXX')
+# selected_pool = service.get_pool_by_uuid(session, pool2.uuid)
+
+# pool_list = service.list_pools(session)
+# print(f'pool_list have {str(len(pool_list))} pools:')
+# for pool in pool_list:
+#     print(pool.to_dict())
+
+# service.delete_pool_by_condition(session, libvirt_pool.to_dict())
+
+# service.delete_pool_by_uuid(session, pool2.uuid)
+
+# print('After delete...')
+
+# pool_list = service.list_pools(session)
+# print(f'pool_list have {str(len(pool_list))} pools:')
+# for pool in pool_list:
+#     print(pool.to_dict())
+
+# session.commit()
+# session.close()
