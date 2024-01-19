e1d0aa51b079554ca3486f240deb69ae1af1c470
George-SSSS <2017312580122@whu.edu.cn>
2024-01-19 08:06:18 +0000

    feat: support return xml

--- backend/src/volume/api.py
+++ backend/src/volume/api.py
@@ -11,47 +11,54 @@ class PoolAPI(object):
 
     def create_pool(self, name: str, allocation: int, user_id: str):
         try:
-            data = pool_service.create_pool(name, allocation, user_id)
-            return APIResponse.success(data)
+            pool = pool_service.create_pool(name, allocation, user_id)
+            return APIResponse.success(pool)
         except Exception as e:
             return APIResponse.error(400, e)
 
     def delete_pool_by_uuid(self, uuid):
         try:
-            data = pool_service.delete_pool_by_uuid(uuid)
-            return APIResponse.success(data)
+            pool_service.delete_pool_by_uuid(uuid)
+            return APIResponse.success(msg='Pool deleted successfully')
         except Exception as e:
             return APIResponse.error(400, e)
 
     def get_pool_by_uuid(self, uuid):
         try:
-            data = pool_service.get_pool_by_uuid(uuid)
-            return APIResponse.success(data)
+            pool = pool_service.get_pool_by_uuid(uuid)
+            return APIResponse.success(pool)
         except Exception as e:
             return APIResponse.error(400, e)
 
     def get_pool_by_name(self, name):
         try:
-            data = pool_service.get_pool_by_name(name)
-            return APIResponse.success(data)
+            pool = pool_service.get_pool_by_name(name)
+            return APIResponse.success(pool)
         except Exception as e:
             return APIResponse.error(400, e)
 
     def list_pools(self):
         try:
-            data = pool_service.list_pools()
-            return APIResponse.success(data)
+            pool_list = pool_service.list_pools()
+            return APIResponse.success(pool_list)
         except Exception as e:
             return APIResponse.error(400, e)
 
 
 class VolumeAPI(object):
 
-    def add_volume_to_guest(self, volume_uuid: str, guest_uuid: str):
+    def add_volume_to_guest(self, volume_uuid: str,
+                            guest_uuid: str,
+                            return_xml: bool = False):
         try:
-            volume_service.add_volume_to_guest(volume_uuid, guest_uuid)
-            return APIResponse.success('The volume is successfully'
-                                       'added to the client.')
+            response = APIResponse.success('The volume is successfully'
+                                           'added to the client.')
+            volume = volume_service.add_volume_to_guest(volume_uuid, guest_uuid)
+            if return_xml:
+                response.set_data(volume.get_xml_string())
+            else:
+                response.set_data(volume)
+            return response
         except Exception as e:
             return APIResponse.error(400, e)
 
@@ -66,72 +73,97 @@ class VolumeAPI(object):
                       pool_uuid: str,
                       volume_name: str,
                       allocation: int,
-                      guest_uuid: str):
+                      guest_uuid: str,
+                      return_xml: bool = False):
         try:
+            response = APIResponse.success()
             volume = volume_service.create_volume(pool_uuid, volume_name,
                                                   allocation, guest_uuid)
-            return APIResponse.success(volume)
+            if return_xml:
+                response.set_data(volume.get_xml_string())
+            else:
+                response.set_data(volume)
+            return response
         except Exception as e:
             return APIResponse.error(400, e)
 
-    def create_from_snap(self, snap_uuid: str, volume_name: str):
+    def create_from_snap(self,
+                         snap_uuid: str,
+                         volume_name: str,
+                         return_xml: bool = False):
         try:
-            data = volume_service.create_from_snap(snap_uuid, volume_name)
-            return APIResponse.success(data)
+            response = APIResponse.success()
+            volume = volume_service.create_from_snap(snap_uuid, volume_name)
+            if return_xml:
+                response.set_data(volume.get_xml_string())
+            else:
+                response.set_data(volume)
+            return response
         except Exception as e:
             return APIResponse.error(400, e)
 
     def clone_volume(self,
                      src_volume_uuid: str,
                      dest_pool_uuid: str,
-                     dest_volume_name: str):
+                     dest_volume_name: str,
+                     return_xml: bool = False):
         try:
+            response = APIResponse.success()
             dest_volume = volume_service.clone_volume(src_volume_uuid,
                                                       dest_pool_uuid,
                                                       dest_volume_name)
-            return APIResponse.success(dest_volume)
+            if return_xml:
+                response.set_data(dest_volume.get_xml_string())
+            else:
+                response.set_data(dest_volume)
+            return response
         except Exception as e:
             return APIResponse.error(400, e)
 
     def delete_volume_by_uuid(self, uuid: str):
         try:
             volume_service.delete_volume_by_uuid(uuid)
-            return APIResponse.success(msg=f'volume deleted successfully')
+            return APIResponse.success(msg='Volume deleted successfully')
         except Exception as e:
             return APIResponse.error(400, e)
 
     def delete_volume_by_name(self, pool_uuid: str, volume_name: str):
         try:
-            data = volume_service.delete_volume_by_name(pool_uuid, volume_name)
-            return APIResponse.success(data)
+            volume_service.delete_volume_by_name(pool_uuid, volume_name)
+            return APIResponse.success(msg='Volume deleted successfully')
         except Exception as e:
             return APIResponse.error(400, e)
 
-    def get_volume_by_uuid(self, uuid: str):
+    def get_volume_by_uuid(self, uuid: str, return_xml: bool = False):
         try:
+            response = APIResponse.success()
             volume = volume_service.get_volume_by_uuid(uuid)
-            return APIResponse.success(volume)
+            if return_xml:
+                response.set_data(volume.get_xml_string())
+            else:
+                response.set_data(volume)
+            return response
         except Exception as e:
             return APIResponse.error(400, e)
 
-    def get_volume_by_name(self, pool_uuid: str, volume_name: str):
+    def get_volume_by_name(self, pool_uuid: str,
+                           volume_name: str,
+                           return_xml: bool = False):
         try:
+            response = APIResponse.success()
             volume = volume_service.get_volume_by_name(pool_uuid, volume_name)
-            return APIResponse.success(volume)
-        except Exception as e:
-            return APIResponse.error(400, e)
-
-    def get_volume_xml(self, volume_uuid: str):
-        try:
-            xml = volume_service.get_volume_xml(volume_uuid)
-            return APIResponse.success(xml)
+            if return_xml:
+                response.set_data(volume.get_xml_string())
+            else:
+                response.set_data(volume)
+            return response
         except Exception as e:
             return APIResponse.error(400, e)
 
     def list_volumes(self, pool_uuid: str):
         try:
-            data = volume_service.list_volumes(pool_uuid)
-            return APIResponse.success(data)
+            volume_list = volume_service.list_volumes(pool_uuid)
+            return APIResponse.success(volume_list)
         except Exception as e:
             return APIResponse.error(400, e)
 
@@ -140,21 +172,21 @@ class SnapshotAPI(object):
 
     def create_snapshot(self, volume_uuid: str, snap_name: str):
         try:
-            data = snap_service.create_snapshot(volume_uuid, snap_name)
-            return APIResponse.success(data)
+            snapshot = snap_service.create_snapshot(volume_uuid, snap_name)
+            return APIResponse.success(snapshot)
         except Exception as e:
             return APIResponse.error(400, e)
 
     def delete_snapshot_by_uuid(self, uuid: str):
         try:
-            data = snap_service.delete_snapshot_by_uuid(uuid)
-            return APIResponse.success(data)
+            snap_service.delete_snapshot_by_uuid(uuid)
+            return APIResponse.success(msg='Snapshot deleted successfully')
         except Exception as e:
             return APIResponse.error(400, e)
 
     def list_snapshots(self, volume_uuid: str = None):
         try:
-            data = snap_service.list_snapshots(volume_uuid)
-            return APIResponse.success(data)
+            snap_list = snap_service.list_snapshots(volume_uuid)
+            return APIResponse.success(snap_list)
         except Exception as e:
             return APIResponse.error(400, e)
