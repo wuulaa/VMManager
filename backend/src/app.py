6d0cb72a2013bf6c8ab66ab78dca33c0edd18a60
root <2406342787@qq.com>
2024-01-25 02:25:42 +0000

    fix: domain_name->domain_uuid

--- backend/src/app.py
+++ backend/src/app.py
@@ -18,7 +18,7 @@ def backend():
 
 @app.route("/test")
 def test():
-    response = requests.get("http://127.0.0.1:5001/test")
+    response = requests.get("http://127.0.0.1:5000/test")
     if response.status_code == 200:
         return response.text
     return "Failed"
