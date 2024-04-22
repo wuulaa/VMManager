c8b43814754bd4c10a51da453221a628fdda437a
york <1627936869@qq.com>
2024-04-22 09:09:14 +0800

    minor: test wsgi server

--- backend_slave/src/app.py
+++ backend_slave/src/app.py
@@ -2,6 +2,11 @@ from flask import Flask
 from src.blueprints.slave_guest_bp import guest_bp
 from src.blueprints.slave_network_bp import network_bp
 from src.utils.config import CONF
+# from waitress import serve
+# import logging
+# from logging.handlers import TimedRotatingFileHandler
+# from gevent.pywsgi import WSGIServer
+
 
 app = Flask(__name__)
 app.register_blueprint(guest_bp)
@@ -13,5 +18,8 @@ def root():
     return "root"
 
 if __name__ == "__main__":
+    
     port = CONF["flask"]["port"]
     app.run(host="0.0.0.0", port=port, debug=False)
+    # http_server = WSGIServer(('0.0.0.0', int(port)), app, log=app.logger)
+    # http_server.serve_forever()
