542c43ac35cb02be069cbe1853ea99a2723b54cd
2406342787@qq.com <2406342787.com>
2024-04-23 17:11:37 +0800

    fix: ceph_init.sh

--- scripts/ceph_init.sh
+++ scripts/ceph_init.sh
@@ -8,5 +8,6 @@ rbd pool init libvirt-pool
 ceph auth get-or-create client.libvirt mon 'profile rbd' osd 'profile rbd pool=libvirt-pool'
 
 #用户授权，每一个节点都需要执行
+virsh secret-define --file secret.xml
 ceph auth get-key client.libvirt | sudo tee client.libvirt.key
-virsh secret-set-value --secret {3510c3cc-37bb-40c7-82cb-b93a6229d22d} --base64 $(cat client.libvirt.key) && rm client.libvirt.key
+virsh secret-set-value --secret 3510c3cc-37bb-40c7-82cb-b93a6229d22d --base64 $(cat client.libvirt.key) && rm client.libvirt.key
