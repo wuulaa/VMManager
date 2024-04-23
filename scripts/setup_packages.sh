b099e123a443e5c07b8f1d12c7fc2b4b8d9332e1
york <1627936869@qq.com>
2024-04-23 15:45:48 +0800

    fix: add pip packages

--- scripts/setup_packages.sh
+++ scripts/setup_packages.sh
@@ -3,3 +3,4 @@
 apt-get install -y git python3.8 python3-pip novnc redis python-is-python3
 apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager
 
+pip install -r requirements.txt
