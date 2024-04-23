dc0b069186ef35063fc3c98d9a207c314dbd22da
york <1627936869@qq.com>
2024-04-23 14:49:27 +0800

    feat:update scripts

--- scripts/setup_packages.sh
+++ scripts/setup_packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt-get install -y git python3.8 python3-pip novnc redis
+apt-get install -y git python3.8 python3-pip novnc redis python-is-python3
 apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager
 
