8ff29f311be46ed9612e8a172f4dc1f1722f978e
2406342787@qq.com <2406342787.com>
2024-05-22 10:15:23 +0800

    feat: update install scripts

--- scripts/setup_packages.sh
+++ scripts/setup_packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y git python3.8 python3-pip novnc redis python-is-python3
+apt-get install -y git python3.8 python3-pip novnc redis python-is-python3 docker
 apt-get install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager
 
 pip install -r requirements.txt
